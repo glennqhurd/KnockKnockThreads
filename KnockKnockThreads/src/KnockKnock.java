9f81ed946da0b80e6148bb2897fd9804a38b0ef2
Glenn Hurd <glennqhurd@gmail.com>
2016-10-08 18:58:00 -0400

    Created main method and 2 classes
    Main method uses the classes Speaker1 and Speaker2 to run separate
    threads to simulate a conversation.  It runs but it does the threads in
    sequence because of synchronized methods.  I'm working on changing the
    run functions to wait until the other run finishes their side of things.
    Still a work in progress though.

--- KnockKnockThreads/src/KnockKnock.java
+++ KnockKnockThreads/src/KnockKnock.java
@@ -1,4 +1,10 @@
 
 public class KnockKnock {
 
+	public static void main(String args[]) {
+		Thread speaker1 = new Thread(new Speaker1());
+		Thread speaker2 = new Thread(new Speaker2());
+		speaker1.run();
+		speaker2.run();
+	}
 }
