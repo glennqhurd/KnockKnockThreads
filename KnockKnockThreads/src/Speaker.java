a9f6c1c5f95b44b4e2faa0aa0a3c12bb4b826a29
Glenn Hurd <glennqhurd@gmail.com>
2016-10-10 09:51:47 -0400

    Implemented setRandomNames and added new lines to speakJokes
    Implemented setRandomNames as a function and called it in Speaker and
    speakJokes instead of only doing it in the constructor.  Added new lines
    to the print statements so the jokes are easier to read.

--- KnockKnockThreads/src/Speaker.java
+++ KnockKnockThreads/src/Speaker.java
@@ -7,6 +7,24 @@ public class Speaker {
 	
 	public Speaker (String[] names) {
 		this.names = names;
+		this.setRandomNames();
+	}
+	
+	public void speakJokes () {
+		String firstSpeaker = String.format("%s: Knock knock!\n", this.currentName1);
+		System.out.println(firstSpeaker);
+		String secondSpeaker = String.format("%s: Who's there?\n", this.currentName2);
+		System.out.println(secondSpeaker);
+		firstSpeaker = String.format("%s: Boo.\n", this.currentName1);
+		System.out.println(firstSpeaker);
+		secondSpeaker = String.format("%s: Boo who?\n", this.currentName2);
+		System.out.println(secondSpeaker);
+		firstSpeaker = String.format("%s: Why are you crying?\n", this.currentName1);
+		System.out.println(firstSpeaker);
+		this.setRandomNames();
+	}
+	
+	public void setRandomNames () {
 		int name1 = ThreadLocalRandom.current().nextInt(0, names.length);
 		this.currentName1 = names[name1];
 		while (currentName2.equals("")) {
@@ -16,21 +34,4 @@ public class Speaker {
 			}
 		}
 	}
-	
-	public void speakJokes () {
-		String firstSpeaker = String.format("%s: Knock knock!", this.currentName1);
-		System.out.println(firstSpeaker);
-		String secondSpeaker = String.format("%s: Who's there?", this.currentName2);
-		System.out.println(secondSpeaker);
-		firstSpeaker = String.format("%s: Boo.", this.currentName1);
-		System.out.println(firstSpeaker);
-		secondSpeaker = String.format("%s: Boo who?", this.currentName2);
-		System.out.println(secondSpeaker);
-		firstSpeaker = String.format("%s: Why are you crying?", this.currentName1);
-		System.out.println(firstSpeaker);
-	}
-	
-	/*public void setRandomNames () {
-		
-	}*/
 }
